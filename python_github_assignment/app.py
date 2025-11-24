095fa60ca2e7ceff9944bdb4b96388da1bd7d09e
lukevandersluis825-stack <lukevandersluis825@gmail.com>
2025-11-24 04:17:34 +0000

    Add calculation based on user input

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -3,3 +3,7 @@ print("Welcome to my Python program!")
 # Ask the user for input (daily study hours)
 hours = input("How many hours did you study today? ")
 
+# Convert the input into a numeric type and perform a calculation
+hours = float(hours)
+weekly_hours = hours * 7
+
